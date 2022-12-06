7610555d968723eb06359941b1187f94b9dd3422
Ryan Lonergan <rlonergan@linode.com>
2022-12-06 15:21:00 -0500

    formatting

--- prom-stack.tf
+++ prom-stack.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     linode = {
-      source = "linode/linode"
+      source  = "linode/linode"
       version = "1.29.4"
     }
   }
@@ -12,32 +12,32 @@ variable "root_password" {}
 variable "linode_api_token" {}
 
 variable "ssh_pubkey" {
-  type = string
+  type    = string
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/X4hgv7AeYtNns7CE0ZCKBjTCRVkxB5ziOc4d82YBuQl3fcPttGnxPCf8SKVEAMtKw+331mXcIL2Yxq+rHaMRDv5r8rwJ/hk3Awyu/Uswe+vpAPFuO3auz2i1tC1NIOqinCCSFLAulsSfCSYn6G+gpEQ1OgdRpD1qQX+QhGjn6P23C731Rw1W1IzO9BSzaqzyLARs8tsC3Q0BPNbsuAgC4f4UmWenJzgU+gaT/8PExLHV1Exs4D8KW+Vvag3DN26oaXDvf4y0eQWuGEOoTs1P6k1nNPO/NE7X0YbB5cFR1TzWJzn4J/PrW90VDv2OmV8K8ZFvQ6FNBsdz0BoXGWILSxp37CiwaTbsdOWIJ9u20jKj19LzpTRB3qKG7fDph03+Re9dXbxNGwJm5INzMZ/89pR+esmZ0/sMgw+0MkeXqSFdsRPatemjhDjMUMVgQOumP+ICfRFU1kDtUZBTzzLnnU6zKlT0PRDuXiLd9vhI7JAknL0xYygh8gbWE9Fjds1ytfNO9I4bpr3O66icMk6631Kx8PyWAlVHJUurHYlL8LC06AFfGhznuph5TVmu/F60ojUQYDyxqNSvjYJVAaF1atlR5qXpWafLW09k4wAippotiVhjWSjpmUUOR2vOC3+M+VYbstW9fBLAe++AJKEK23QYwXwMTptR4YsyFmf02w== rlonergan@rlonergan-C02ZW8AVMD6M"
 }
 
 provider "linode" {
-  token = "${var.linode_api_token}"
+  token = var.linode_api_token
 }
 
 data "linode_profile" "me" {}
 
 resource "linode_instance" "prometheus" {
-  image = "linode/debian11"
-  label = "prometheus"
-  group = "prom-stack"
-  region = "us-southeast"
-  type = "g6-dedicated-2"
-  tags = [ "prom-stack" ]
-  private_ip = true
-  authorized_users = [ data.linode_profile.me.username ]
-  root_pass = "${var.root_password}"
-  stackscript_id = 1095232
+  image            = "linode/debian11"
+  label            = "prometheus"
+  group            = "prom-stack"
+  region           = "us-southeast"
+  type             = "g6-dedicated-2"
+  tags             = ["prom-stack"]
+  private_ip       = true
+  authorized_users = [data.linode_profile.me.username]
+  root_pass        = var.root_password
+  stackscript_id   = 1095232
   stackscript_data = {
-    "username": "prometheus",
-    "password": "${var.root_password}",
-    "pubkey": "${var.ssh_pubkey}",
-    "disable_root": "No"
+    "username" : "prometheus",
+    "password" : "${var.root_password}",
+    "pubkey" : "${var.ssh_pubkey}",
+    "disable_root" : "No"
   }
 
   provisioner "file" {
@@ -59,7 +59,7 @@ resource "linode_instance" "prometheus" {
   provisioner "file" {
     source      = "./configs/prometheus.yml"
     destination = "/etc/prometheus/prometheus.yml"
-  }  
+  }
 
   provisioner "file" {
     source      = "./configs/prometheus.service"
@@ -84,8 +84,8 @@ resource "linode_instance" "prometheus" {
   connection {
     type     = "ssh"
     user     = "root"
-    password = "${var.root_password}"
-    host     = "${self.ip_address}"
+    password = var.root_password
+    host     = self.ip_address
   }
 
   provisioner "remote-exec" {
@@ -97,11 +97,11 @@ resource "linode_instance" "prometheus" {
 }
 
 resource "linode_domain_record" "prometheus-A" {
-    domain_id = 1395459
-    name = "prom-test"
-    record_type = "A"
-    target = linode_instance.prometheus.ip_address
-    ttl_sec = 30
+  domain_id   = 1395459
+  name        = "prom-test"
+  record_type = "A"
+  target      = linode_instance.prometheus.ip_address
+  ttl_sec     = 30
 }
 
 # resource "linode_domain_record" "prometheus-AAAA" {
@@ -116,8 +116,8 @@ resource "null_resource" "configure_ssl" {
   connection {
     type     = "ssh"
     user     = "root"
-    password = "${var.root_password}"
-    host     = "${linode_instance.prometheus.ip_address}"
+    password = var.root_password
+    host     = linode_instance.prometheus.ip_address
   }
 
   provisioner "remote-exec" {
